25cbf47eac6f713cd2646ef5bf6cdce2bd8e6588
Sarah Wiseman <s.wiseman777@gmail.com>
2024-04-02 16:05:50 +0100

    quick accessibility challenge

---
+++ app.js
@@ -0,0 +1,10 @@
+const links = document.querySelector("a");
+
+links.addEventListener("focus", (e) => {
+    
+    e.target.style.color = "tomato";
+})
+// links.forEach(function (link) {
+  
+//   });
+// });
