59452ba352e9059a99bba102d601a9fe2a215ada
Sarah Wiseman <s.wiseman777@gmail.com>
2024-04-02 21:22:16 +0100

    accessibility updates

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const links = document.querySelector("a");
+// const links = document.querySelector("a");
 
-links.addEventListener("focus", (e) => {
-    
-    e.target.style.color = "tomato";
-})
+// links.addEventListener("focus", (e) => {
+
+//     e.target.style.color = "tomato";
+// })
 // links.forEach(function (link) {
-  
+
 //   });
 // });
+
+const links = document.querySelectorAll("h1, a");
+
+links.forEach(function (link) {
+  link.addEventListener("focus", (e) => {
+    e.target.style.color = "tomato";
+    console.log(`Element ${e.currentTarget} is focused`);
+  });
+});
